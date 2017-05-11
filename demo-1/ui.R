4c8184777caa8097a5fcf642e1edc654a32babae
Utako Kase <utakok@uw.edu>
2017-05-11 15:18:14 -0700

    ex2 completed

--- demo-1/ui.R
+++ demo-1/ui.R
@@ -5,7 +5,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Hello Shiny!"),
+  titlePanel("My Very First Demo"),
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
@@ -13,8 +13,8 @@ shinyUI(fluidPage(
       sliderInput("bins",
                   "Number of bins:",
                   min = 1,
-                  max = 50,
-                  value = 30)
+                  max = 100,
+                  value = 50)
     ),
     
     # Show a plot of the generated distribution
--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -1,15 +1,15 @@
 # UI for scatterplot
-
+library(shiny)
 # Create a shinyUI with a fluidPage layout
 shinyUI(fluidPage(
-  
+  titlePanel("Hello"),
   # Add a numbericInput to choose the number of observations
-  
+  numericInput("num", "Input", 10, min = 1, max = 100),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput("color", "Color", list("Black" = "black", "Blue" = "blue", "Red" = "red")),
   
   # Plot the output with the name "scatter"
-  
+  plotOutput('scatterPlot')
   
 ))
